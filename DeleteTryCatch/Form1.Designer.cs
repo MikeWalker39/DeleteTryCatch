afc40611b1f7f29e3644056c7ac9d65d0ca3c2a1
Mike Walker <mike.walker3939@gmail.com>
2020-08-24 07:17:23 -0500

    Add project files.

---
+++ DeleteTryCatch/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿using DevExpress.XtraEditors;
+
+namespace DeleteTryCatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbCatch = new System.Windows.Forms.RichTextBox();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.SuspendLayout();
+            // 
+            // rtbCatch
+            // 
+            this.rtbCatch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbCatch.Location = new System.Drawing.Point(0, 49);
+            this.rtbCatch.Name = "rtbCatch";
+            this.rtbCatch.Size = new System.Drawing.Size(873, 936);
+            this.rtbCatch.TabIndex = 2;
+            this.rtbCatch.Text = "";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.simpleButton1.Location = new System.Drawing.Point(0, 0);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(873, 49);
+            this.simpleButton1.TabIndex = 3;
+            this.simpleButton1.Text = "simpleButton1";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 985);
+            this.Controls.Add(this.rtbCatch);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox rtbCatch;
+        private SimpleButton simpleButton1;
+    }
+}
+
---
+++ DeleteTryCatch/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeleteTryCatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click_1(object sender, EventArgs e)
+        {
+            string s = Clipboard.GetText();
+            
+            StringBuilder sb = new StringBuilder();
+
+            int lineNumber = 0;
+
+            List<string> lines = s.Split('\n').ToList();
+
+
+            bool skipOneLine = false;
+            bool skipTilClose = false;
+
+            string buffer = "";
+
+            foreach(string line in lines)
+            {
+
+                if (skipOneLine)
+                {
+                    buffer = "";
+
+                    skipOneLine = false;
+
+                    continue;
+                }
+
+                if (skipTilClose)
+                {
+                    buffer = "";
+
+                    if (line.Contains("}"))
+                    {
+                        skipTilClose = false;
+                    }
+
+                    continue;                    
+                }
+
+                if (line.Trim() == "try")
+                {
+                    sb.AppendLine(buffer);
+
+                    buffer = "";
+
+                    skipOneLine = true;
+
+                    continue;
+                }
+
+                if (line.Trim() == "catch (Exception ex)")
+                {
+                    skipTilClose = true;
+
+                    continue;
+                }
+                                
+                sb.AppendLine(buffer);
+
+                buffer = line;
+            }
+
+            sb.AppendLine(buffer);
+
+            StringBuilder sb2 = new StringBuilder();
+
+            bool wasBlank = false;
+
+           
+
+            foreach(string str in sb.ToString().Split('\n'))
+            {
+                bool isBlank = false;
+
+                if (str.Trim().Length == 0)
+                {
+                    isBlank = true;
+                }
+
+                if (wasBlank == true && isBlank == true) continue;
+
+                wasBlank = isBlank;
+
+                sb2.AppendLine(str);
+            }
+                
+
+            Clipboard.SetText(sb2.ToString().Replace("\r", ""));
+
+            rtbCatch.Text = sb2.ToString();
+        }
+    }
+}
+
